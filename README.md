f97e408f4932ab54ce5cc7cb760e2f23499cad2a
ArseniyFokin <arseniifokin@gmail.com>
2021-05-15 01:39:01 +0300

    add README

---
+++ README.md
@@ -0,0 +1,14 @@
+#Text to PDF
+
+Start:
+1. pip install fpdf
+2. python app.py --method {file, files, folder} --input [PATH ...] --output PATH
+
+-m {files,file,folder}, --method {files,file,folder}
+                        How to read text
+
+-i INPUT [INPUT ...], --input INPUT [INPUT ...]
+                        File/files/folder path/paths
+
+-o OUTPUT, --output OUTPUT
+                        File path to save PDF
--- arg_parser.py
+++ arg_parser.py
@@ -2,13 +2,13 @@ from argparse import ArgumentParser
 
 class ArgParser:
     def __init__(self):
-        self.root = ArgumentParser(description='Program for translating the test to PDF')
+        self.root = ArgumentParser(description='Program for translating the text to PDF')
         self.add_arguments()
 
     def add_arguments(self):
         self.root.add_argument("-m", "--method", choices=["files", "file", "folder"], type=str, help="How to read text", required=True)
-        self.root.add_argument("-i", "--input", nargs='+', type=str, help="File/files/folder path", required=True)
-        self.root.add_argument("-o", "--output", type=str, help="File path", required=True)
+        self.root.add_argument("-i", "--input", nargs='+', type=str, help="File/files/folder path/paths", required=True)
+        self.root.add_argument("-o", "--output", type=str, help="File path to save PDF", required=True)
 
     def start_parse(self):
         return self.root.parse_args()
